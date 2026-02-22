25409ce9a7f086b443e68fbcf9de0c8ebb3b07d8
Gareth Widlansky <gareth.widlansky@proton.me>
2026-02-21 21:45:08 -0500

    cleanups, actually works

--- context.scm
+++ context.scm
@@ -21,8 +21,14 @@
                      [else (loop pred vec (add1 idx))]))])
     (loop pred lst 0)))
 
+(define (tree-eq? a b)
+  (let ([a-root (tstree->root a)]
+        [b-root (tstree->root b)])
+    (and (= (tsnode-end-byte a-root) (tsnode-end-byte b-root))
+         (= (tsnode-start-byte a-root) (tsnode-start-byte b-root)))))
+
 (define (get-query lang)
-  (let* ([filepath (string-append "/queries/" lang ".tsq")]
+  (let* ([filepath (string-append "queries/" lang ".tsq")]
          [exists (path-exists? filepath)]
          [pos (get-pos (lambda (x) (string=? (first x) lang)) cached-queries)])
     (cond
@@ -50,30 +56,34 @@
   (letrec ([trees (get-trees-at-cursor doc-id)]
            [loop
             (lambda (lst acc)
-              (cond
-                [(empty? lst) acc]
-                [(member (first lst) (map ConMatch-tree matches))
-                 (let ([idx (get-pos (lambda (x) (equal? (ConMatch-tree x) (first lst))) matches)])
-                   (set-status! "skibidi")
-                   (loop (rest lst) (cons (list-ref matches idx) acc)))]
-                ;; root tree doesn't have query? don't bother.
-                [(TSQuery? (get-query (tstree->language (first lst))))
-                 (loop (rest lst)
-                       (let* ([tree (first lst)]
-                              [root (tstree->root tree)]
-                              [match (query-document-byte-range
-                                      query-loader
-                                      doc-id
-                                      (tsnode-start-byte root)
-                                      ;; skibidi finess
-                                      (begin
-                                        ; (set-status! (number->string (sub1 (tsnode-end-byte root))))
-                                        (sub1 (tsnode-end-byte root))))]
-                              [named (filter (lambda (x) (equal? (tsnode->tstree x) tree))
-                                             (tsmatch-capture match "context"))]
-                              [surrounding (filter (lambda (x) (equal? (tsnode->tstree x) tree))
-                                                   (tsmatch-capture match "context.name"))])
-                         (loop (rest lst) (cons (ConMatch tree named surrounding) acc))))]))])
+              (if (empty? lst)
+                  acc
+                  (let ([idx (get-pos (lambda (x) (tree-eq? (ConMatch-tree x) (first lst))) matches)])
+                    (cond
+                      [(number? idx)
+                       (begin
+                         (set-status! "yipee, cached")
+                         (loop (rest lst) (cons (list-ref matches idx) acc)))]
+                      ;; root tree doesn't have query? don't bother.
+                      [(TSQuery? (get-query (tstree->language (first lst))))
+                       (begin
+                         (loop
+                          (rest lst)
+                          (let* ([tree (first lst)]
+                                 [root (tstree->root tree)]
+                                 [match (query-document-byte-range query-loader
+                                                                   doc-id
+                                                                   (tsnode-start-byte root)
+                                                                   ;; skibidi finess
+                                                                   (sub1 (tsnode-end-byte root)))]
+                                 [named (filter (lambda (x) (tree-eq? (tsnode->tstree x) tree))
+                                                (tsmatch-capture match "context"))]
+                                 [surrounding (filter (lambda (x) (tree-eq? (tsnode->tstree x) tree))
+                                                      (tsmatch-capture match "context.name"))])
+                            (cons (ConMatch tree surrounding named) acc))))]
+                      [else (loop (rest lst) acc)]))))])
+
+    ; (set-status! trees)
     (loop trees '())))
 
 (define (tsnode-text-slice node text)
@@ -90,30 +100,31 @@
         (set! cached-match (get-contexts doc-id cached-match)))))
 
 (define (get-path match text pos)
-  (if (and (not (empty? match)))
+  (if (empty? match)
+      '()
       (foldr (lambda (x acc)
                (append (map (lambda (y) (rope->string (tsnode-text-slice (second y) text)))
                             (filter (lambda (z)
-                                      ((let ([surrounding (first z)])
-                                         (and (<= pos (tsnode-end-byte surrounding))
-                                              (>= pos (tsnode-start-byte surrounding))))))
-                                    (transduce (ConMatch-surrouding x)
-                                               (zipping (ConMatch-nodes x))
+                                      (let ([surrounding (first z)])
+                                        (and (<= pos (tsnode-end-byte surrounding))
+                                             (>= pos (tsnode-start-byte surrounding)))))
+                                    (transduce (ConMatch-nodes x)
+                                               (zipping (ConMatch-surrouding x))
                                                (into-list))))
                        acc))
              '()
-             match)
-      '()))
+             match)))
 
 (define (set-path! doc-id)
   (set! path
         (string-join (let* ([text (editor->text doc-id)]
                             [pos (rope-char->byte text (cursor-position))]
                             [tree (document->tree-byte-range doc-id pos pos)])
-                       (when (and (TSTree? tree)
-                                  (not (= (tsnode-start-byte (tstree->root tree)) cached-tree-start)))
-                         (set! cached-tree-start (tsnode-start-byte (tstree->root tree)))
-                         (refresh-context-query!))
+                       (if (and (TSTree? tree)
+                                (not (= (tsnode-start-byte (tstree->root tree)) cached-tree-start)))
+                           (begin
+                             (set! cached-tree-start (tsnode-start-byte (tstree->root tree)))
+                             (refresh-context-query!)))
                        (get-path cached-match text pos))
                      " > ")))
 
@@ -127,8 +138,6 @@
                           (style-with-bold (style))))))
 
 (define (context-enable side)
-  (register-hook! "document-changed" (lambda (_ _) (refresh-context-query!)))
-  (register-hook! "document-focus-lost" (lambda (_) (refresh-context-query!)))
   (push-status-element! side context-status-element))
 
 (provide context-status-element
