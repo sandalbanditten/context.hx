b56ad4f052366bdaa97ea113fc39ba33204436d0
Gareth Widlansky <gareth.widlansky@proton.me>
2026-02-26 14:53:01 -0500

    colon

--- context.scm
+++ context.scm
@@ -88,7 +88,7 @@
                     (if focused
                         (sep (map (lambda (x) (span x (style)))
                                   (get-path cached-match (editor->doc-id view-id)))
-                             (span " ⇒ " (style-with-dim (theme-scope-ref "keyword"))))))))
+                             (span " : " (style-with-dim (theme-scope-ref "keyword"))))))))
 
 (define (context-enable side)
   (register-hook 'document-changed (lambda (doc-id _) (refresh-context-query! doc-id)))
